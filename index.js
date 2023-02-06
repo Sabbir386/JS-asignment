2656931e16b0cb676c24ff9086ab602401b0162f
Sabbir386 <sabbir386.sa@gmail.com>
2023-02-07 00:28:26 +0600

    addedd mindGame solution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
  /*JS Project*/
+ function mindGame(num){
+    if(typeof num!=='number' || num<0 ){
+        return 'Input Should be a Positive Number';
+    }
+    num=(((num*3+10)/2)-5);
+    return num;
+ }
+//  console.log(mindGame(3));
